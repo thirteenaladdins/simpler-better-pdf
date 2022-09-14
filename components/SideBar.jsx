83c5fff9a5cd4e3c98284f88ca2ad71cb10b6199
thirteenaladdins <moamrr@gmail.com>
2022-09-14 23:54:54 +0100

    sidebar added

--- components/SideBar.jsx
+++ components/SideBar.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import { React, useState, setState } from 'react';
+import MenuItems from './MenuItems';
 
-export default function SideBar() {
+// when the item is clicked - update the state of the parent component
+// now we should pass the state to the parent component?
+// only one can be true at a time
+
+// option
+// add selected state / true or false
+export default function SideBar({ setOption }) {
   return (
     <div className="sidebar mx-auto pt-8 font-sans">
-      Sidebar
       {/* list item  */}
+      <ul>
+        {MenuItems.map((item, index) => (
+          <li key={index}>
+            <button type="button" className={item.cName} onClick={() => { setOption(item.title); }}>
+              {item.title}
+            </button>
+          </li>
+        ))}
+      </ul>
 
     </div>
   );
