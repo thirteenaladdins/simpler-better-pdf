fb296f029850e4f09df0bb6bd658c990203d1414
thirteenaladdins <moamrr@gmail.com>
2023-03-18 18:39:34 +0000

    removed all errors

--- nextjs-frontend/.eslintrc.js
+++ nextjs-frontend/.eslintrc.js
@@ -5,29 +5,34 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    // 'plugin:next/recommended',
+    'plugin:@next/next/recommended',
     'plugin:react/recommended',
+    'eslint:recommended',
     'airbnb-typescript',
   ],
   parser: '@typescript-eslint/parser',
   overrides: [
   ],
   parserOptions: {
-    ecmaVersion: 'latest',
+    // ecmaVersion: 'latest',
     project: 'tsconfig.json',
   },
   plugins: [
     'react',
+    'react-hooks',
     'import',
+    // 'next',
+    'jsx-a11y',
   ],
   rules: {
     'linebreak-style': 'off',
-    // '@typescript-eslint/indent': ['error', 2],
+    '@typescript-eslint/indent': 'off',
     // 'react/jsx-filename-extension': ['error', { extensions: ['.jsx', '.tsx'] }],
   },
   settings: {
     react: {
-      version: 'detect'
+      version: 'detect',
     },
-  }
+  root: true,
+  },
 };
