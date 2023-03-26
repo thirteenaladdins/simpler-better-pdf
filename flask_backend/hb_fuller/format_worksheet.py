e4ee7b6b07d956f8331ba74929ca6e564c7b7c32
thirteenaladdins <moamrr@gmail.com>
2023-03-26 17:23:09 +0100

    format worksheet updated

--- flask_backend/hb_fuller/format_worksheet.py
+++ flask_backend/hb_fuller/format_worksheet.py
@@ -1,92 +1,80 @@
-import sys, os
+import sys
+import os
 import pandas as pd
-import numpy as np
-from prompt_toolkit import prompt
-from prompt_toolkit.completion import WordCompleter
 
-# Get the absolute path to the parent directory of the current file
+# Set up directories
 parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-
-# Add the parent directory and worksheet_validation directory to the Python path
-sys.path.append(parent_dir)
 worksheet_validation_dir = os.path.join(parent_dir, 'worksheet_validation')
+
+sys.path.append(parent_dir)
 sys.path.append(worksheet_validation_dir)
 
+# Import custom modules
 from worksheet_validation.row_schema import row_data_schema
 from worksheet_validation.header_schema import headers_schema
 
-# print(sys.path)
-# from ..utils.helpers import load_file
-# from worksheet_validation.row_schema 
-# from ..worksheet_validation.row_schema import row_data_schema
+# Read input file and clean data
+input_file = sys.argv[1]
+df = pd.read_excel(input_file, dtype=str)
+valid_row_threshold = 10
+df_cleaned = df.dropna(thresh=valid_row_threshold)
 
-# sys.path.append(os.path.abspath(os.path.join('..', 'worksheet_validation')))
-# from ..worksheet_validation.row_schema import row_data_schema
-
-# # from "../row_schema" import row_schema_data 
-
-# # this isn't going to be a cmd line script anyway
-# # if len(sys.argv) != 2:
-
-# df = pd.read_excel(sys.argv[1], dtype=str)
-# print(df)
-# # read worksheets, select worksheet to parse information
-
-# # Open the Excel file using the ExcelFile class
-excel_file = pd.ExcelFile(sys.argv[1])
-
-# # Get a list of the sheet names
+# Get sheet names
+excel_file = pd.ExcelFile(input_file)
 sheet_names = excel_file.sheet_names
 
-# # TODO: in frontend choose which worksheet you want to format
-print(sheet_names)
+# Define EU countries
+eu_countries = ['AT', 'BE', 'BG', 'HR', 'CY', 'CZ', 'DK', 'EE', 
+                'FI', 'FR', 'DE', 'GR', 'HU', 'IE', 'IT', 'LV',
+                'LT', 'LU', 'MT', 'NL', 'PL', 'PT', 'RO', 'SK',
+                'SI', 'ES', 'SE']
 
-# # Read the first sheet into a pandas dataframe
-df = excel_file.parse(sheet_names[0], dtype=str)
+# Select relevant columns
+selected_columns = ["Plant Name", "Currency", "General description", "Qty Shipped Net Weight KG", 
+                    "Sales Order Nbr", "Number Pieces", "Date Creation Record", "Customs Value", 
+                    "10-Digit UK Import", "Country Of Origin", "EORI Nbr"]
+df_subset = df_cleaned[selected_columns]
 
-# filter by week and by delivery terms. group them all together.
-
-# # input worksheet
-# if 912/914 use the following headers
-# if 3rd party use the other headers
-
-# Define a custom function to classify each country of origin value
-def classify_country_of_origin(country):
-    if country in ['AT', 'BE', 'BG', 'HR', 'CY', 'CZ', 'DK', 'EE', 
-                    'FI', 'FR', 'DE', 'GR', 'HU', 'IE', 'IT', 'LV',
-                      'LT', 'LU', 'MT', 'NL', 'PL', 'PT', 'RO', 'SK',
-                        'SI', 'ES', 'SE']:
-        return 'Country of Preferential Origin'
-    else:
-        return 'Origin Country'
-
-
-header_names = "Plant Nbr", "Plant Name", "Sales Order Nbr", "Sales Order Item Nbr", "Customer Nbr", "Customer Name", \
-                "Material Nbr", "Material Name", "General description", "Qty Shipped Net Weight KG", \
-                "Packaging Net Weight", "Number Pieces", "Material Type", "Material value", "Freight Cost", \
-                "Customs Value", "Currency", "Date Shipped", "Date Creation Record", "Estimated arrival date", \
-                "Calendar Week", "Incoterms", "Commodity Code", "10-Digit UK Import", "Country Of Origin", \
-                "Union status", "Duty free - TCA", "Grouped Deliveries", "Delivery Type Name", "Sales Org", \
-                "Named Importer", "Name of Importer", "EORI Nbr", "Importer VAT Nbr", "Customs procedure code"
-
-
-# output excel sheet mapping columns from excel to CDS worksheet columns
-df_subset = df[["Plant Name", "Currency", "General description", "Qty Shipped Net Weight KG", 
-  "Sales Order Nbr", "Number Pieces", "Date Creation Record", "Customs Value", 
-  "10-Digit UK Import", "Country Of Origin", "EORI Nbr"]]
-
-row_data_schema.keys()
-
-# Create an empty DataFrame with the column headers
-
+# Create DataFrames for headers and rows
 headers_df = pd.DataFrame(columns=headers_schema.keys())
 rows_df = pd.DataFrame(columns=row_data_schema.keys())
 
-# add additional columns with the same names
-# this should be outside of this script? This is a primitive way to do it
+def add_address_data(row):
+    pass
 
-# rows_df['Line Number'] = range(1, len(rows_df) + 1)
 
+# Function to categorize country and create new rows
+def categorize_country(row):
+    new_rows = {'row1': {}, 'row2': {}}
+
+    if row['Country Of Origin'] in eu_countries:
+        new_rows['row1'] = {
+            'Country of Preferential Origin': row['Country Of Origin'],
+            'Preference': 300,
+            'Doc Type': 'U112',
+            'Doc Status': 'JP',
+            'Doc Reference': row['Sales Order Nbr'] + ' ' + row['Date Creation Record'],
+            'Doc Reason': 'IMPORTERS KNOWLEDGE',
+        }
+    else:
+        new_rows['row1'] = {
+            'Origin Country': row['Country Of Origin'],
+            'Preference': 100,
+            'Tax Type': 'A00',
+            'MoP': 'E',
+            'Doc Type': 'C505',
+            'Doc Status': 'CC',
+            'Doc Reference': 'GBCGUGARANTEENOTREQUIRED',
+        }
+
+        new_rows['row2'] = {
+            'Doc Type': 'C506',
+            'Doc Reference': 'GBDPO8115401',
+        }
+
+    return new_rows
+
+# Set up basic row data
 rows_df["Goods Description"] = df_subset["General description"]
 rows_df["Currency"] = df_subset["Currency"]
 rows_df["Net Mass"] = df_subset["Qty Shipped Net Weight KG"]
@@ -102,124 +90,45 @@ rows_df["Package Kind"] = "PK"
 rows_df["Package Number"] = df_subset["Number Pieces"]
 rows_df["Package Marks"] = "As addressed"
 
+# Generate new rows based on country categorization
+new_rows_list = []
+
+for index, row in df_subset.iterrows():
+    new_rows = categorize_country(row)
+    new_rows_list.append(new_rows)
+
+# Add new row data to rows_df
+for index, new_rows in enumerate(new_rows_list):
+    for column, value in new_rows['row1'].items():
+        if column in rows_df.columns:
+            rows_df.at[index, column] = value
+
+# Create rows2_df with the data from new_rows['row2']
+row2_list = [rows['row2'] for rows in new_rows_list]
+rows_df_2 = pd.DataFrame(row2_list)
+
 rows_df["Prev Doc Category"] = "Z"
 rows_df["Prev Doc Type"] = "380"
-
-# format the date properly
 rows_df["Prev Doc Reference"] = df_subset['Sales Order Nbr'].str.cat(df['Date Creation Record'], sep=' ')
 
-# define the list of EU countries
-# eu_countries = ['Austria', 'Belgium', 'Bulgaria', 'Croatia', 'Cyprus', 
-#                 'Czech Republic', 'Denmark', 'Estonia', 'Finland', 'France', 'Germany',
-#                   'Greece', 'Hungary', 'Ireland', 'Italy', 'Latvia', 'Lithuania',
-#                   'Luxembourg', 'Malta', 'Netherlands', 'Poland', 'Portugal',
-#                     'Romania', 'Slovakia', 'Slovenia', 'Spain', 'Sweden']
-
-# eu_countries = ['AT', 'BE', 'BG', 'HR', 'CY', 'CZ', 'DK', 'EE', 
-#                     'FI', 'FR', 'DE', 'GR', 'HU', 'IE', 'IT', 'LV',
-#                       'LT', 'LU', 'MT', 'NL', 'PL', 'PT', 'RO', 'SK',
-#                         'SI', 'ES', 'SE']
-
-eu_countries = ['AT', 'BE', 'BG', 'HR', 'CY', 'CZ', 'DK', 'EE', 
-                    'FI', 'FR', 'DE', 'GR', 'HU', 'IE', 'IT', 'LV',
-                      'LT', 'LU', 'MT', 'NL', 'PL', 'PT', 'RO', 'SK',
-                        'SI', 'ES', 'SE']
-
-def categorize_country(country):
-    if country in eu_countries:
-        return 'Country of Preferential Origin'
-    else:
-        return 'Origin Country'
-
-origin_df = pd.DataFrame(index=df_subset.index)
-origin_df =  df_subset['Country Of Origin'].apply(categorize_country)
-
-print(origin_df)
-
-df_subset['Country Of Origin']
-
-
-
-
-
-
-# view the modified rows_df
-# print(rows_df)
-
-
-
-# create duplicate columns
-
-# create new df
-
-# so you have to specify the number of rows
-rows_df_2 = pd.DataFrame(index=df_subset.index)
-rows_df_2["Prev Doc Category"] = "Y"
-rows_df_2["Prev Doc Type"] = "CLE"
-rows_df_2["Prev Doc Reference"] = df_subset["Date Creation Record"]
-rows_df_2["Doc Type"] = "N935"
-rows_df_2["Doc Status"] = "AE"
-rows_df_2["Doc Reference"] = df_subset['Sales Order Nbr'].str.cat(df['Date Creation Record'], sep=' ')
-
-# Country Of Origin
-
-# define the dataframe
-# df = pd.DataFrame({'Country': ['France', 'Spain', 'Australia', 'Italy']})
-
-
-
-# rows_df_3 = pd.DataFrame(index=df_subset.index)
-# rows_df_3["Doc Type"] = "U112"
-# rows_df_3["Doc Status"] = "JP"
-# rows_df_3["Doc Reference"] = df_subset['Sales Order Nbr'].str.cat(df['Date Creation Record'], sep=' ')
-# rows_df_3["Doc Reason"] = "Importers Knowledge"
+rows_df_3 = pd.DataFrame(index=df_subset.index)
+rows_df_3["Prev Doc Category"] = "Y"
+rows_df_3["Prev Doc Type"] = "CLE"
+rows_df_3["Prev Doc Reference"] = df_subset["Date Creation Record"]
+rows_df_3["Doc Type"] = "N935"
+rows_df_3["Doc Status"] = "AE"
+rows_df_3["Doc Reference"] = df_subset['Sales Order Nbr'].str.cat(df['Date Creation Record'], sep=' ')
 
 rows_df_4 = pd.DataFrame(index=df_subset.index)
 rows_df_4["Doc Type"] = "C514"
-rows_df_4["Doc Reference"] = "EIDR REF"
+rows_df_4["Doc Reference"] = "GBEIR570487130006I20220104092202"
 
-# TODO: 
-# 100, 300
-# countries of origin
-# check plant name and assign addresses
+# Concatenate all row DataFrames
+final_rows_df = pd.concat([rows_df, rows_df_2, rows_df_3, rows_df_4], axis=1)
+print(final_rows_df)
 
-# add A00 - E for non EU
-# add C505, C506 for non EU and dutiable 
-
-# header page should be the only thing you have to fill out
-
-# add commodity validation for 
-# check table first, if not present then fetch from the api. 
-# but then if the data is present in the table but has been updated this won't work
-
-
-# depends on the origin
-# rows_df[] = df_subset["Country Of Origin"]
-
-# Write the DataFrame to an Excel file
+# Write output to Excel file
 with pd.ExcelWriter('output.xlsx') as writer:
     headers_df.to_excel(writer, sheet_name='Header', index=False)
-    rows_df.to_excel(writer, sheet_name='Rows', index=False)
+    final_rows_df.to_excel(writer, sheet_name='Rows', index=False)
 
-
-# print(df_subset)
-
-
-
-# Plant Name - to exporter - match data from table
-
-# General description   to description
-# Qty Shipped Net Weight KG   - net weight gross weight
-
-# Sales Order Nbr - Z 380 previous doc
-# Number Pieces   - packages
-# Date Creation Record   - Y CLE date + to invoice
-
-# Customs Value   - value
-
-# 10-Digit UK Import   - commodity
-
-# Country Of Origin    - origin country of preferential origin
-
-# "EORI Nbr", "Importer VAT Nbr"
-# check that this matches 912 or 914 so we don't double enter the data like it happened that time
