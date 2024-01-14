6b46ad92ff7d860d635341d080d22cddbf4f1296
Mo Amer <moamrr@gmail.com>
2024-01-14 22:16:42 +0000

    fixed FOUC issue in frontend

--- svelte-frontend/src/routes/+layout.svelte
+++ svelte-frontend/src/routes/+layout.svelte
@@ -7,6 +7,14 @@
 	// import { theme } from '../store/themeStore';
 	import { theme } from '../store/themeStore';
 
+	import { onMount } from 'svelte';
+
+	// Body is initially hidden in app.css and unhidden here to avoid FOUC
+	onMount(() => {
+		// This code runs when the component is mounted in the DOM
+		document.body.style.visibility = 'visible';
+	});
+
 	$: if (typeof window !== 'undefined' && $theme) {
 		document.documentElement.setAttribute('data-theme', $theme.actual);
 	}
